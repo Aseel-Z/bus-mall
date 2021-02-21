076c844486f838923f2fa9046ac33fb98b77e275
Aseel-Z <aseelalzweri@gmail.com>
2021-02-21 17:35:52 +0200

    V1.1

--- app.js
+++ app.js
@@ -14,11 +14,49 @@
 // For each of the three images, increment its property of times it has been shown by one.
 // Attach an event listener to the section of the HTML page where the images are going to be displayed.
 // Once the users ‘clicks’ a product, generate three new products for the user to pick from.
-
+let products = [];
 function Product(name,image,display) {
 this.productName = name;
 this.imagePath = image;
-this.displayCount = display;
-this.push
-
+this.displayCount = 0;
+products.push(this);
 }
+
+new Product('bag','img/bag.jpg',this.displayCount);
+new Product('banana','img/banana.jpg',this.displayCount);
+new Product('bathroom','img/bathroom.jpg',this.displayCount);
+new Product('boots','img/boots.jpg',this.displayCount);
+new Product('breakfast','img/breakfast.jpg',this.displayCount);
+new Product('bubblegum','img/bubblegum.jpg',this.displayCount);
+new Product('chair','img/chair.jpg',this.displayCount);
+new Product('cthulhu','img/cthulhu.jpg',this.displayCount);
+new Product('dog-duck','img/dog-duck.jpg',this.displayCount);
+new Product('dragon','img/dragon.jpg',this.displayCount);
+new Product('pen','img/pen.jpg',this.displayCount);
+new Product('pet-sweep','img/pet-sweep.jpg',this.displayCount);
+new Product('scissors','img/scissors.jpg',this.displayCount);
+new Product('shark','img/shark.jpg',this.displayCount);
+new Product('sweep','img/sweep.jpg',this.displayCount);
+new Product('tauntaun','img/tauntaun.jpg',this.displayCount);
+new Product('unicorn','img/unicorn.jpg',this.displayCount);
+new Product('usb','img/usb.jpg',this.displayCount);
+new Product('water-can','img/water-can.jpg',this.displayCount);
+new Product('wine-glass','img/wine-glass.jpg',this.displayCount);
+
+
+
+// this.displayCount gets updated;increments by 1,
+//  each time the associated images is displayed randomly, if img url contains name then +1
+// let c = 0;
+// for (let i = 0; i < products.length; i++) {
+//     console.log(products[i]);
+//     c+=1;
+// }
+// console.log(c)
+
+for (let j = 1; j <= 3; j++) {
+ let imageRandom = Math.ceil(Math.random());
+ console.log(imageRandom)
+ console.log(products[imageRandom].imagePath);
+}
+
